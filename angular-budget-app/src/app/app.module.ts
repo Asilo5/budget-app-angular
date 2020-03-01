8a62f54d1130ecdd32aaf5e126a9d0e9f4c947ae
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-01 15:39:29 -0700

    Add budget list component and connect it to main to make sure it works

--- angular-budget-app/src/app/app.module.ts
+++ angular-budget-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddBudgetFormComponent } from './add-budget-form/add-budget-form.component';
+import { BudgetListComponent } from './budget-list/budget-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    AddBudgetFormComponent
+    AddBudgetFormComponent,
+    BudgetListComponent
   ],
   imports: [
     BrowserModule,
