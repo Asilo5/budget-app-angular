0cdc37b77ea3e671a5868766df6e7d11558e7e56
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-01 16:17:03 -0700

    Add new component where outputs of the budget form will appear

--- angular-budget-app/src/app/app.module.ts
+++ angular-budget-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddBudgetFormComponent } from './add-budget-form/add-budget-form.component';
 import { BudgetListComponent } from './budget-list/budget-list.component';
+import { BudgetInputComponent } from './budget-list/budget-input/budget-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     AddBudgetFormComponent,
-    BudgetListComponent
+    BudgetListComponent,
+    BudgetInputComponent
   ],
   imports: [
     BrowserModule,
