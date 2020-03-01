7f539e781521342b9b1a7c07214f14404e6d0cec
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-01 14:20:03 -0700

    Add last styling to overall budget and add new components for the form

--- angular-budget-app/src/app/app.module.ts
+++ angular-budget-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AddBudgetFormComponent } from './add-budget-form/add-budget-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    AddBudgetFormComponent
   ],
   imports: [
     BrowserModule,
